9685f79f4286ac6ff563e767e1166a4ae43e8057
Neider Ruiz <neiderbtc@gmail.com>
2025-01-30 00:36:06 -0500

    text manipulation logic

---
+++ src/texts/extract-keys-and-texts.ts
@@ -0,0 +1,13 @@
+import { generateHashText } from "translate-projects-core/utils";
+
+export const extractKeysAndTexts = async (content: string): Promise<Record<string, string>> => {
+    const regex = /{{([\s\S]*?)}}/g;
+    let match;
+    const result: Record<string, string> = {};
+
+    while ((match = regex.exec(content)) !== null) {
+        result[await generateHashText(match[1])] = match[1];
+    }
+
+    return result;
+}
---
+++ src/texts/index.ts
@@ -0,0 +1,4 @@
+export * from './escape-characters';
+export * from './extract-keys-and-texts';
+export * from './replace-content-file';
+
