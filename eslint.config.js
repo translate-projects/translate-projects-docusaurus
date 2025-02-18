ed4179ba82fc86144679c78bad5edec9a47ec1b8
Neider Ruiz <neiderbtc@gmail.com>
2025-02-18 17:59:00 -0500

    remove packages unused

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,18 @@
-// eslint.config.js
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
-
 export default [
   {
-    files: ['**/*.ts'],
+    ignores: ['node_modules', 'dist'],
+  },
+  {
     languageOptions: {
-      parser: tsParser,
       ecmaVersion: 'latest',
       sourceType: 'module',
     },
     plugins: {
-      prettier: eslintPluginPrettier,
-      '@typescript-eslint': tseslint,
+      prettier: require('eslint-plugin-prettier'),
     },
     rules: {
-      ...eslintConfigPrettier.rules,
-      ...tseslint.configs.recommended.rules,
+      ...require('eslint-config-prettier').rules,
       'prettier/prettier': 'error',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      'no-console': 'off',
     },
   },
 ];
